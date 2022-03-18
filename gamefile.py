565f1a04bf1e62f4343d1997a1ef337ac8e57c3c
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-03-18 09:55:31 -0400

    Create gamefile.py

---
+++ gamefile.py
@@ -0,0 +1,11 @@
+import random
+import os
+import getpass
+
+def get_users():
+  
+
+def create_user():
+  username = str(input("Enter Username: "))
+  password = getpass.getpass(prompt="Enter Password: ")
+  get_users()
