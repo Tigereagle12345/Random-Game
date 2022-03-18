7ca028f99aa0a7b415630959e6bbfacfa33dae5a
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-03-18 12:03:00 -0400

    Create Installer.bat

---
+++ Installer.bat
@@ -0,0 +1,12 @@
+echo WARNING: This program may need to be run with administrator privelidges.
+echo Beginning Installation...
+
+echo 
+echo Installing Python Modules...
+
+pip install os
+pip install random
+pip install json
+pip install getpass
+
+echo Done
